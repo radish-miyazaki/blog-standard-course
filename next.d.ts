b49e5fa9427ed7ccbf8975c825cf59b7c909c0ac
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2023-05-04 18:10:17 +0900

    overload AppProps and impl getAppProps method

--- next.d.ts
+++ next.d.ts
@@ -1,7 +1,7 @@
 // ref: https://dev.to/ofilipowicz/next-js-per-page-layouts-and-typescript-lh5
 // official docs: https://nextjs.org/docs/basic-features/layouts#with-typescript
 import type { NextComponentType, NextPageContext, NextLayoutComponentType } from 'next';
-import type { AppProps } from 'next/app';
+import type { AppProps } from '../utils/getAppProps';
 
 declare module 'next' {
   type NextLayoutComponentType<P = {}> = NextComponentType<NextPageContext, any, P> & {
